8b79347c4df7230d6dbcf5e486fe215ec44d9d13
sourabhch13 <sourabh.c@consultadd.com>
2024-01-17 00:35:44 +0530

    Implemented Update and Delete route fro Department

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from .views import DepartmentView
 
 urlpatterns = [
     path('/department', DepartmentView.as_view()),
+    path('/department/<str:id>',DepartmentView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,14 @@
 from rest_framework import status
 from rest_framework.views import APIView,Response
 from .models import Department
-
+from . serializers import DepartmentSerializer
 # Create your views here.
 
 class DepartmentView(APIView):
 
     #get all the Departmemts
     def get(self,request):
-        dept=list(Department.objects.all().values())
+        dept=DepartmentSerializer(Department.objects.all(),many=True).data
         
         response_data={
             "success": True,
@@ -38,3 +38,40 @@ class DepartmentView(APIView):
             return (response_data,status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return Response(response_data,status.HTTP_201_CREATED)
+    
+    def put(self,request,id):
+        
+        dept = Department.objects.filter(id=id).first()
+        response_data={
+            "success": True,
+            "message": "Department Updated succesfully!"
+        }
+        if dept is None:
+            response_data['success']=False,
+            response_data['message']='Department not found!'
+            return Response(response_data,status.HTTP_404_NOT_FOUND)
+
+        for key,value in request.data.items():
+            setattr(dept,key,value)
+
+        dept.save()
+
+        return Response(response_data,status.HTTP_204_NO_CONTENT)
+    
+    def patch(self,request,id):
+        return self.put(request,id)
+    
+
+    def delete(self,request,id):
+        dept = Department.objects.get(id=id)
+        response_data={
+            "success": True,
+            "message": "Department Deleted succesfully!"
+        }
+        if dept is None:
+            response_data['success']=False,
+            response_data['message']='Department not found!'
+            return Response(response_data,status.HTTP_404_NOT_FOUND)
+        
+        dept.delete()
+        return Response(response_data,status.HTTP_204_NO_CONTENT)
