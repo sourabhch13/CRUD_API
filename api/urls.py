d34ca5ac69636b8c6949224011e1b636613d6539
sourabhch13 <sourabh.c@consultadd.com>
2024-01-17 01:04:09 +0530

    added employee model and routes for CRUD

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import DepartmentView
-
-
-urlpatterns = [
-    path('/department', DepartmentView.as_view()),
-    path('/department/<str:id>',DepartmentView.as_view()),
-]
